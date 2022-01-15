550f15d55e15d02b95496c9f1a2785a0da64e518
KelvinS <kelvinpfw@gmail.com>
2022-01-15 14:50:34 -0300

    Change check_order script to consider nested menus

--- check_order.py
+++ check_order.py
@@ -1,16 +1,40 @@
 
+def check_order(lines, match_pattern, stop_pattern):
+    """Check the order based on the patterns."""
+    data = list()
+    for line in lines:
+        if line.startswith(match_pattern):
+            data.append(line)
+        elif line.startswith(stop_pattern):
+            if data != sorted(data, key=str.casefold):
+                print(data)
+                print(sorted(data, key=str.casefold))
+                raise Exception('The content is not alphabetically sorted!')
+            data = list()
+
+
+def check_menu(lines, level):
+    """Check if menu is alphabetically sorted."""
+    match_pattern = f'{" " * level}-'
+    stop_pattern = ('---', '- [')
+    check_order(lines, match_pattern, stop_pattern)
+
+
+def check_content(lines):
+    """Check if content is alphabetically sorted."""
+    match_pattern = '* ['
+    stop_pattern = ('## ', '### ')
+    check_order(lines, match_pattern, stop_pattern)
+
+
 def main(path):
     """Check if menus are alphabetically sorted."""
     data = list()
     with open(path, 'r') as f:
         lines = f.readlines()
-    for line in lines:
-        if line.startswith(('    ', '* [')):
-            data.append(line)
-        elif line.startswith(('- [', '## ', '### ')):
-            if data != sorted(data, key=str.casefold):
-                raise Exception('The content is not alphabetically sorted!')
-            data = list()
+    check_menu(lines, level=4)
+    check_menu(lines, level=8)
+    check_content(lines)
 
 
 if __name__ == '__main__':
